85f043a913d6a952d5eb97555784a74faf9f1ae0
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-19 12:55:41 -0400

    Update 04.sum numbers with exceptions.py

--- code/04.sum numbers with exceptions.py
+++ code/04.sum numbers with exceptions.py
@@ -1,1 +1,6 @@
+Calculate the sum from 0 to a given number N, excluding numbers that are divisible by 3 and 7.
 
+Constraints
+The input variable N must be an integer.
+N must be non-negative.
+N can be any positive integer, including zero.
