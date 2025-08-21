a85651f317bfe8a2b3c9a3e70af66598cb3c20f4
ADITH SREERAM A S <73573214+ASR373@users.noreply.github.com>
2025-08-20 23:56:05 -0400

    Update 05. Replace Nulls with Preceding Non-nulls.py

--- code/05. Replace Nulls with Preceding Non-nulls.py
+++ code/05. Replace Nulls with Preceding Non-nulls.py
@@ -1,3 +1,16 @@
+Given a list of values containing NULLs, for every null value, replace it with its preceding non-null value.
+
+Constraints
+The input variable lst is a list.
+The elements of lst can be either integers or None.
+The length of lst can be any positive integer.
+The list lst may contain multiple consecutive None values.
+The list lst may start with a None value.
+The list lst may end with a None value.
+
+
+sol:
+
 
 def replace_null_values(lst):
     for i in range(1, len(lst)):
